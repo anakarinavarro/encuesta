4b869aee8db2000a398c3b6ca8ae5ce508f7ccf0
PABLO ADONIS <pabloadonis@MacBook-Pro-de-PABLO.local>
2022-04-15 17:58:05 -0400

    first commit

---
+++ assets/app.js
@@ -0,0 +1,92 @@
+
+const preguntas = [
+
+    { PREGUNTA : 1,
+    A : 1,
+    B: 1,
+    C: 1,
+    D: 1,
+    },
+    {
+        PREGUNTA: 2,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 3,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 4,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 5,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 6,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 7,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 8,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 9,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 10,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 11,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+    {
+        PREGUNTA: 12,
+        A:1,
+        B:1,
+        C:1,
+        D:1,
+    },
+
+]
+
+console.log(preguntas[10].B);
+
+//const dirigir = preguntas[]
